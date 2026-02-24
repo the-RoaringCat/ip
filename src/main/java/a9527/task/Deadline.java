4bae5c63ffe514dfd589b45674ec8ec3e317c839
Mo Hanqi Laptop <mohanqi666@gmail.com>
2026-02-24 15:43:12 +0800

    Add ability to standardise certain date formats to MMM dd yyyy.

--- src/main/java/a9527/task/Deadline.java
+++ src/main/java/a9527/task/Deadline.java
@@ -1,32 +1,38 @@
 package a9527.task;
+
+import a9527.date.Date;
+
+import java.time.LocalTime;
+
 public class Deadline extends Task{
-    protected String by;
+    //protected String by;
+    private Date by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = new Date(by);
     }
 
     public Deadline(String description, boolean isDone, String by) {
         super(description, isDone);
-        this.by = by;
+        this.by = new Date(by);
     }
 
     public void setBy(String by) {
-        this.by = by;
+        this.by = new Date(by);
     }
 
-    public String getBy() {
+    public Date getBy() {
         return by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.toString() + ")";
     }
 
     @Override
     public String toStorageFormat() {
-        return "D | " + super.toStorageFormat() + " | " + by;
+        return "D | " + super.toStorageFormat() + " | " + by.toString();
     }
 }
--- src/main/java/a9527/task/Event.java
+++ src/main/java/a9527/task/Event.java
@@ -1,43 +1,46 @@
 package a9527.task;
+
+import a9527.date.Date;
+
 public class Event extends Task{
-    String start;
-    String end;
+    private Date start;
+    private Date end;
 
     public Event(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        this.start = new Date(start);
+        this.end =  new Date(end);
     }
 
     public Event(String description, boolean isDone,  String start, String end) {
         super(description, isDone);
-        this.start = start;
-        this.end = end;
+        this.start = new Date (start);
+        this.end = new Date (end);
     }
 
     public void setStart(String start) {
-        this.start = start;
+        this.start = new Date(start);
     }
 
     public void setEnd(String end) {
-        this.end = end;
+        this.end = new Date (end);
     }
 
-    public String getStart() {
+    public Date getStart() {
         return start;
     }
 
-    public String getEnd() {
+    public Date getEnd() {
         return end;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
+        return "[E]" + super.toString() + " (from: " + start.toString() + " to: " + end.toString() + ")";
     }
 
     @Override
     public String toStorageFormat() {
-        return "E | " + super.toStorageFormat() + " | " + start + " | " + end;
+        return "E | " + super.toStorageFormat() + " | " + start.toString() + " | " + end.toString();
     }
 }
