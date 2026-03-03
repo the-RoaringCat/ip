2cc528abc0910196d992a21b1e6b0bea4df73fc3
Mo Hanqi <mohanqi666@gmail.com>
2026-03-04 03:04:20 +0800

    Ui fix

--- src/main/java/a9527/a9527/A9527.java
+++ src/main/java/a9527/a9527/A9527.java
@@ -26,13 +26,13 @@ public class A9527 {
      */
     public static void greetUser() {
         String logo = """
-                \t  ___  ____  ____  _____
+                  ___  ____  ____  _____
                 \t / _ \\| ___||___ \\|___  |
                 \t| (_) |___ \\  __) |  / /\s
                 \t \\__/ |___) )/ __/_ / /
                 \t   /_/|____/|_____//_/
                 """;
-        Ui.print("Hello I'm\n" + logo + "\tWhat you want me to do now?");
+        Ui.print("Hello I'm" + System.lineSeparator() + logo + "\tWhat you want me to do now?");
     }
 
     /**
